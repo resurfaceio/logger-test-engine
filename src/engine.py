d653e5eaa8fa13c1240eae950d54b8edc1b9f763
Anyesh <sthacruz@gmail.com>
2021-07-30 15:29:17 +0545

    feat(init-engine): :sparkles: Update engine and add aysnc calls

--- src/engine.py
+++ src/engine.py
@@ -1,45 +1,72 @@
-from utils import yaml_loader, run_gql_query
-import requests
+import asyncio
 import json
 
-payloads = yaml_loader("API.yaml")["payloads"]
+import httpx
+
+from .settings import BASEDIR, logger
+from .utils import parse_args, run_gql_query, yaml_loader
+
+payloads = yaml_loader(BASEDIR / "API.yaml")["payloads"]
+loggers = yaml_loader(BASEDIR / "config.yaml")
 
 placeholder = "RESURFACE_PLACEHOLDER"
-original_val = "localhost:8000"
+original_url = "localhost:8000"
 
 
-def main():
+async def create_task(payload, test_apps):
+    msg = "Request completed succesfully!"
+    task = None
+    results = []
+
+    async with httpx.AsyncClient() as requests:
+        tasks = []
+        for app in test_apps:
+            url_ = payload.get("url").replace(placeholder, app.get("url"))
+            logger.info(f"Running url '{url_}' for app '{app.get('name')}'")
+            if payload.get("type", None) == "GQL":
+                task = requests.post(url_, json={"query": payload.get("request_body")})
+
+            else:
+                task = requests.get(url_)
+
+            tasks.append(task)
+
+        reqs = await asyncio.gather(*tasks)
+
+        for req in reqs:
+
+            results.append(
+                {
+                    "url": url_,
+                    "message": str(msg),
+                    "success": (
+                        json.dumps(json.loads(payload.get("response_body")))
+                        == json.dumps(req.json())
+                    ),
+                }
+            )
+    return results
+
+
+def main(request=None):
     results = []
     msg = None
     response = None
+    if request is None:
+        request = {"logger": "python"}
+
+    request_params = parse_args(request)
+    logger_ = request_params.get("logger")
+    logger.info(f"Running test for '{logger_}' logger")
+    test_apps = loggers.get(str(logger_).lower())["apps"]
+    logger.info(f"There are {len(test_apps)} test apps for '{logger_}' logger")
 
     for payload in payloads:
-        URL = payload.get("url").replace(placeholder, original_val)
-        try:
-            if payload.get("type", None) == "GQL":
-                response = run_gql_query(
-                    url=URL,
-                    query=payload.get("request_body"),
-                )
-            else:
-                response = requests.get(URL).json()
-            msg = "Request completed succesfully!"
-        except Exception as e:
-            msg = e
 
-        results.append(
-            {
-                "url": URL,
-                "message": msg,
-                "success": (
-                    json.dumps(json.loads(payload.get("response_body")))
-                    == json.dumps(response)
-                ),
-            }
-        )
-    return results, all(x["success"] for x in results)
+        done, extra = asyncio.run(create_task(payload, test_apps))
+        print(done)
+        print(extra)
 
 
 if __name__ == "__main__":
-    _, all_good = main()
-    print(_)
+    main()

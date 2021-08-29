38e2269d2062a30a089878daafaf288e4a3111f4
Anyesh <sthacruz@gmail.com>
2021-08-29 11:10:02 +0545

    feat: Add CLI setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+def read_file(name):
+    with open(name) as fd:
+        return fd.read()
+
+
+setup(
+    name="test-resurface",
+    version="0.1.0",
+    description=("Resurfaceio integration test "),
+    author="Anish Shrestha",
+    author_email="hello@anyesh.xyz",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=read_file("requirements.txt").splitlines(),
+    entry_points={
+        "console_scripts": ["test-resurface=src.engine:cli"],
+    },
+    package_data={"": ["config.yaml", "README.md"]},
+    include_package_data=True,
+    zip_safe=False,
+)

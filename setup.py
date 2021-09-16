6348e5f559494d9be9b7ab64a58f2fa47ae0b85c
Anish Shrestha <26220465+Anyesh@users.noreply.github.com>
2021-09-16 10:16:53 +0545

    chore: Change author details

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     name="test-resurface",
     version="0.1.0",
     description=("Resurfaceio integration test "),
-    author="Anish Shrestha",
-    author_email="hello@anyesh.xyz",
+    author="resurfaceio",
     packages=find_packages(exclude=["tests"]),
     install_requires=read_file("requirements.txt").splitlines(),
     entry_points={
